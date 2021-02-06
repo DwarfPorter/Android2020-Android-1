cdaf2c53b055129864408dbe8e5543a13d2a956f
DwarfPorter <mrvlmor@gmail.com>
2021-02-06 15:18:33 +0300

    FragmentNavigation: Добавление главного меню

--- Lesson-7/FragmentNavigation/app/src/main/java/ru/geekbrains/fragmentnavigation/MainActivity.java
+++ Lesson-7/FragmentNavigation/app/src/main/java/ru/geekbrains/fragmentnavigation/MainActivity.java
@@ -1,8 +1,12 @@
 package ru.geekbrains.fragmentnavigation;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -129,6 +133,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         fragmentTransaction.commit();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Обработка выбора пункта меню приложения (активити)
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.action_settings:
+                addFragment(new SettingsFragment(), TAG_SETTINGS);
+                return true;
+            case R.id.action_main:
+                addFragment(new MainFragment(), TAG_MAIN);
+                return true;
+            case R.id.action_favorite:
+                addFragment(new FavoriteFragment(), TAG_FAVORITE);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Здесь определяем меню приложения (активити)
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.action_search); // поиск пункта меню поиска
+        SearchView searchText = (SearchView) search.getActionView(); // строка поиска
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // реагирует на конец ввода поиска
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            // реагирует на нажатие каждой клавиши
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
         return true;
     }
 }
